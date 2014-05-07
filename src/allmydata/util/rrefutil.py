e2882e9a387114052e2314d1c54dec49e84f348e
David Stainton <dstainton415@gmail.com>
2014-05-07 21:23:17 +0000

    Fix location hints format incompatibility error

--- src/allmydata/util/rrefutil.py
+++ src/allmydata/util/rrefutil.py
@@ -3,6 +3,30 @@ from twisted.internet import address
 from foolscap.api import Violation, RemoteException, DeadReferenceError, \
      SturdyRef
 
+import re
+
+# TODO: set this from tahoe.cfg? or something less ugly...
+only_tor = True
+
+CONVERT_ENDPOINT_RE=re.compile(r"^[^:]+:(.+)")
+
+def get_host_from_endpoint(endpointDesc):
+    mo = CONVERT_ENDPOINT_RE.search(endpointDesc)
+    if mo:
+        vars = mo.group(1).split(':')
+        host = vars[0]
+        if '=' in host:
+            for var in vars:
+                # BUG: check for error
+                key, value = var.split('=')
+                if key == 'host':
+                    return value
+        else:
+            return host
+    else:
+        raise ValueError("failed to parse twisted endpoint descriptor")
+
+
 def add_version_to_remote_reference(rref, default):
     """I try to add a .version attribute to the given RemoteReference. I call
     the remote get_version() method to learn its version. I'll add the
@@ -31,25 +55,22 @@ def hosts_for_rref(rref, ignore_localhost=True):
     # actually, this only returns hostnames
     advertised = []
     for hint in rref.getLocationHints():
-        # Foolscap-0.2.5 and earlier used strings in .locationHints, but we
-        # require a newer version that uses tuples of ("ipv4", host, port)
-        assert not isinstance(hint, str), hint
-        if hint[0] == "ipv4":
-            host = hint[1]
-            if ignore_localhost and host == "127.0.0.1":
-                continue
-            advertised.append(host)
+        assert isinstance(hint, str)
+        host = get_host_from_endpoint(hint)
+        if ignore_localhost and host == "127.0.0.1":
+            continue
+        advertised.append(host)
     return advertised
 
 def hosts_for_furl(furl, ignore_localhost=True):
     advertised = []
     for hint in SturdyRef(furl).locationHints:
-        assert not isinstance(hint, str), hint
-        if hint[0] == "ipv4":
-            host = hint[1]
-            if ignore_localhost and host == "127.0.0.1":
-                continue
-            advertised.append(host)
+        assert isinstance(hint, str)
+        host = get_host_from_endpoint(hint)
+        if ignore_localhost and host == "127.0.0.1":
+            continue
+        advertised.append(host)
+
     return advertised
 
 def stringify_remote_address(rref):
