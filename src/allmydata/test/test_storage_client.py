5c943cd60ef16ed325da9066ee3d57580832c4ff
David Stainton <dstainton415@gmail.com>
2015-04-06 19:21:13 +0000

    Add test for StorageFarmBroker max connection thresh

--- src/allmydata/test/test_storage_client.py
+++ src/allmydata/test/test_storage_client.py
@@ -1,5 +1,6 @@
 
 from twisted.trial import unittest
+from twisted.internet import defer
 from allmydata.storage_client import NativeStorageServer
 
 
@@ -28,3 +29,13 @@ class TestNativeStorageServer(unittest.TestCase):
             })
         self.failUnlessEqual(nss.get_available_space(), 111)
 
+class TestStorageFarmBroker(unittest.TestCase):
+    def test_connected_thresh(self):
+
+        connected_deferred = defer.Deferred()
+        sb = StorageFarmBroker(None, True, 1, connected_deferred)
+        for k in ["%d" % i for i in range(5)]:
+            ann = {"anonymous-storage-FURL": "pb://abcde@nowhere/fake",
+                   "permutation-seed-base32": base32.b2a(k) }
+        sb.test_add_rref(k, "rref", ann)
+        self.failUnlessEqual(broker.get_connected_servers(), 5)
