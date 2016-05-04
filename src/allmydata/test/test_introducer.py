851f0aa53a521da198bbbda4c16cf454bc18da95
David Stainton <dstainton415@gmail.com>
2016-05-04 10:50:27 +0000

    Wrap long lines in test_introducer.py

--- src/allmydata/test/test_introducer.py
+++ src/allmydata/test/test_introducer.py
@@ -124,7 +124,8 @@ class Introducer(ServiceMixin, unittest.TestCase, pollmixin.PollMixin):
         i = IntroducerService()
         ic = IntroducerClient(None,
                               "introducer.furl", u"my_nickname",
-                              "my_version", "oldest_version", {}, fakeseq, FilePath("notexist.introduced.yaml"))
+                              "my_version", "oldest_version", {}, fakeseq,
+                              FilePath("notexist.introduced.yaml"))
         sk_s, vk_s = keyutil.make_keypair()
         sk, _ignored = keyutil.parse_privkey(sk_s)
         keyid = keyutil.remove_prefix(vk_s, "pub-v0-")
@@ -173,7 +174,8 @@ class Client(unittest.TestCase):
     def test_duplicate_receive_v1(self):
         ic = IntroducerClient(None,
                               "introducer.furl", u"my_nickname",
-                              "my_version", "oldest_version", {}, fakeseq, FilePath("notexist.introduced.yaml"))
+                              "my_version", "oldest_version", {}, fakeseq,
+                              FilePath("notexist.introduced.yaml"))
         announcements = []
         ic.subscribe_to("storage",
                         lambda key_s,ann: announcements.append(ann))
@@ -222,12 +224,14 @@ class Client(unittest.TestCase):
     def test_duplicate_receive_v2(self):
         ic1 = IntroducerClient(None,
                                "introducer.furl", u"my_nickname",
-                               "ver23", "oldest_version", {}, fakeseq, FilePath("notexist.introduced.yaml"))
+                               "ver23", "oldest_version", {}, fakeseq,
+                               FilePath("notexist.introduced.yaml"))
         # we use a second client just to create a different-looking
         # announcement
         ic2 = IntroducerClient(None,
                                "introducer.furl", u"my_nickname",
-                               "ver24","oldest_version",{}, fakeseq, FilePath("notexist.introduced.yaml"))
+                               "ver24","oldest_version",{}, fakeseq,
+                               FilePath("notexist.introduced.yaml"))
         announcements = []
         def _received(key_s, ann):
             announcements.append( (key_s, ann) )
@@ -330,7 +334,8 @@ class Client(unittest.TestCase):
         # not replace the other)
         ic = IntroducerClient(None,
                               "introducer.furl", u"my_nickname",
-                              "my_version", "oldest_version", {}, fakeseq, FilePath("notexist.introduced.yaml"))
+                              "my_version", "oldest_version", {}, fakeseq,
+                              FilePath("notexist.introduced.yaml"))
         announcements = []
         ic.subscribe_to("storage",
                         lambda key_s,ann: announcements.append(ann))
@@ -368,7 +373,8 @@ class Server(unittest.TestCase):
         i = IntroducerService()
         ic1 = IntroducerClient(None,
                                "introducer.furl", u"my_nickname",
-                               "ver23", "oldest_version", {}, realseq, FilePath("notexist.introduced.yaml"))
+                               "ver23", "oldest_version", {}, realseq,
+                               FilePath("notexist.introduced.yaml"))
         furl1 = "pb://62ubehyunnyhzs7r6vdonnm2hpi52w6y@127.0.0.1:36106/gydnp"
 
         privkey_s, _ = keyutil.make_keypair()
@@ -470,7 +476,8 @@ class Queue(SystemTestMixin, unittest.TestCase):
         tub2 = Tub()
         tub2.setServiceParent(self.parent)
         c = IntroducerClient(tub2, ifurl,
-                             u"nickname", "version", "oldest", {}, fakeseq, FilePath("notexist.introduced.yaml"))
+                             u"nickname", "version", "oldest", {}, fakeseq,
+                             FilePath("notexist.introduced.yaml"))
         furl1 = "pb://onug64tu@127.0.0.1:123/short" # base32("short")
         sk_s, vk_s = keyutil.make_keypair()
         sk, _ignored = keyutil.parse_privkey(sk_s)
@@ -558,7 +565,8 @@ class SystemTest(SystemTestMixin, unittest.TestCase):
                 c = IntroducerClient(tub, self.introducer_furl,
                                      NICKNAME % str(i),
                                      "version", "oldest",
-                                     {"component": "component-v1"}, fakeseq, FilePath("notexist.introduced.yaml"))
+                                     {"component": "component-v1"}, fakeseq,
+                                     FilePath("notexist.introduced.yaml"))
             received_announcements[c] = {}
             def got(key_s_or_tubid, ann, announcements, i):
                 if i == 0:
